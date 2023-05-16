ba9f926197e29723f31ea3d05e2fc7c17439b653
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2023-05-16 16:20:26 +0300

    Add algorithm

--- src/main/java/common/Configuration.java
+++ src/main/java/common/Configuration.java
@@ -6,5 +6,6 @@ package common;
 public class Configuration {
     public static final String IP = "127.0.0.1";
     public static final int PORT = 8087;
+    public static final String CONFIG = "MD5";
 
 }
