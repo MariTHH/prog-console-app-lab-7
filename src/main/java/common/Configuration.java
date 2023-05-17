69c8e9f48c771817c8f9da05b250e9c2a4f91628
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2023-05-17 18:03:02 +0300

    Add host port helios

--- src/main/java/common/Configuration.java
+++ src/main/java/common/Configuration.java
@@ -7,5 +7,10 @@ public class Configuration {
     public static final String IP = "127.0.0.1";
     public static final int PORT = 8087;
     public static final String CONFIG = "MD5";
+    public static final String DB_HOST = "pg";
+    public static final int DB_PORT = 2908;
+    public static final boolean HELIOS = true;
 
+    public static final String jdbcLocal = String.format("jdbc:postgresql://%s:%d/studs", HELIOS ? DB_HOST : IP, HELIOS ? 5432 : DB_PORT);
 }
+
