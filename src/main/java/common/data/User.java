315a62fb52675c49bffbfb4765cc1f962391fa2d
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2023-05-17 18:00:31 +0300

    Add class for user

---
+++ src/main/java/common/data/User.java
@@ -0,0 +1,29 @@
+package common.data;
+
+public class User {
+    private static final long serialVersionUID = 0x0A0A0A;
+
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
