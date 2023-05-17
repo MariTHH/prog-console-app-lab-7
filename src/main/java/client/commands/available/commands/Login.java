2fe9a4ecfecb668d4ff08106de776a1832ca9e79
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2023-05-17 17:58:14 +0300

    Add class for login user

---
+++ src/main/java/client/commands/available/commands/Login.java
@@ -0,0 +1,46 @@
+package client.commands.available.commands;
+
+import client.ReadManager;
+import client.RequestManager;
+import client.commands.Command;
+import common.Confident;
+import common.data.User;
+import common.network.CommandResult;
+import common.network.Request;
+
+import java.util.Scanner;
+
+public class Login extends Command {
+
+    public Login(RequestManager requestManager) {
+        super(requestManager);
+    }
+
+    @Override
+    public String getName() {
+        return "login";
+    }
+
+    @Override
+    public String getDescription() {
+        return "проверка пользователя";
+    }
+
+    @Override
+    public void execute(String[] args) {
+        if (args.length > 1) {
+            System.out.println("Вы неправильно ввели команду");
+        } else {
+            Scanner scanner = new Scanner(System.in);
+            String username = ReadManager.readName(scanner);
+            String password = ReadManager.takePassword(scanner);
+            password = Confident.encode(password);
+            User user = new User(username, password);
+            Request<User> request = new Request<>(getName(), user, null);
+            CommandResult result = requestManager.sendRequest(request);
+            if (result.status) {
+                requestManager.setUser(user);
+            }
+        }
+    }
+}
