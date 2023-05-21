bab7def34e5b8217499a9912cd6ed02bb6c9fb6d
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2023-05-21 21:07:03 +0300

    Add check username

--- src/main/java/client/commands/available/commands/Register.java
+++ src/main/java/client/commands/available/commands/Register.java
@@ -1,9 +1,14 @@
 package client.commands.available.commands;
 
+import client.ReadManager;
 import client.RequestManager;
+import client.commands.Command;
+import common.Confident;
 import common.data.User;
 import common.network.CommandResult;
 import common.network.Request;
+
+import java.util.Scanner;
 
 public class Register extends Login{
     public Register(RequestManager requestManager) {
@@ -15,10 +20,28 @@ public class Register extends Login{
         return "register";
     }
 
-    public void execute(){
-        Request<User> request = new Request<>(getName(), null, null);
-        CommandResult result = requestManager.sendRequest(request);
-        System.out.println(result.message);
+    public void execute(String[] args) {
+        boolean flag = false;
+        while (!flag) {
+            Scanner scanner = new Scanner(System.in);
+            String username = ReadManager.readName(scanner);
+            Request<String> request1 = new Request<>("check_register", username, null);
+            CommandResult result1 = requestManager.sendRequest(request1);
+            System.out.println(result1.message);
+            if (result1.status) {
+                flag = true;
+                String password = ReadManager.takePassword(scanner);
+                password = Confident.encode(password);
+                User user = new User(username, password);
+                Request<User> request = new Request<>(getName(), user, null);
+                CommandResult result = requestManager.sendRequest(request);
+                if (result.status) {
+                    requestManager.setUser(user);
+                }
+                System.out.println(result.message);
+            }
+
+        }
     }
 
     @Override
